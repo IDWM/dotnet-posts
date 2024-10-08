4f32b71372c3fff6d925ae7ee3ad6a48ed733eda
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-10-08 20:23:08 -0300

    feat: add get posts

--- Src/Controllers/PostController.cs
+++ Src/Controllers/PostController.cs
@@ -1,7 +1,24 @@
+using dotnet_images.Src.DTOs;
+using dotnet_images.Src.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
 namespace dotnet_images.Src.Controllers
 {
-    public class PostController : BaseApiController
+    public class PostController(IPostRepository postRepository) : BaseApiController
     {
-        
+        private readonly IPostRepository _postRepository = postRepository;
+
+        [HttpGet]
+        public async Task<IActionResult> GetPosts()
+        {
+            var posts = await _postRepository.GetPostsAsync();
+            return Ok(posts);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreatePost(CreatePostDto createPostDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
