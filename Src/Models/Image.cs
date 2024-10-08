da5729d9365f4bdcc3db076c0633a4a50dd1b12c
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-10-08 20:01:54 -0300

    feat: add image model

---
+++ Src/Models/Image.cs
@@ -0,0 +1,11 @@
+namespace dotnet_images.Src.Models
+{
+    public class Image
+    {
+        public long Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required string Url { get; set; }
+        public required string PublicId { get; set; }
+    }
+}
