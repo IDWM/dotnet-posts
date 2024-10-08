523e87998bc808a828c330dd37f8ed71874860f2
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-10-08 20:04:29 -0300

    feat: add data context

---
+++ Src/Data/DataContext.cs
@@ -0,0 +1,10 @@
+using dotnet_images.Src.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_images.Src.Data
+{
+    public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+    {
+        public DbSet<Image> Images { get; set; }
+    }
+}
