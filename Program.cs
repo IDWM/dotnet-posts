5100ec8a17785f15c79cb2ff68c52d1e55fc91f3
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-10-08 20:08:16 -0300

    feat: set default database connection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using dotnet_images.Src.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
