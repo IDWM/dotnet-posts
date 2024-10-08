c5367da51cbf3dc0d7328a75a1cdfb83cf8546ef
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-10-08 20:11:15 -0300

    feat: create base api controller

---
+++ Src/Controllers/BaseApiController.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_images.Src.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase;
+}
