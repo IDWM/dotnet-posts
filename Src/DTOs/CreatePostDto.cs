5e34012ab365c0b972117d5dca3e5a2c45fca292
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-10-09 22:14:02 -0300

    feat: add attributes

--- Src/DTOs/CreatePostDto.cs
+++ Src/DTOs/CreatePostDto.cs
@@ -1,4 +1,9 @@
 namespace dotnet_posts.Src.DTOs
 {
-    public class CreatePostDto { }
+    public class CreatePostDto
+    {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required IFormFile Image { get; set; }
+    }
 }
--- Src/DTOs/PostDto.cs
+++ Src/DTOs/PostDto.cs
@@ -2,6 +2,9 @@ namespace dotnet_posts.Src.DTOs
 {
     public class PostDto
     {
-        
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required string Url { get; set; }
+        public required string PublicId { get; set; }
     }
 }
